4420e5d66b639434122bd3453d5ba4fbd1cb942c
Bryanayrb <Bryan.s.pawlowski@gmail.com>
2014-06-03 02:13:08 -0700

    quick update

---
+++ SSS/SSS/subSurface.hlsl
@@ -0,0 +1,12 @@
+
+typedef struct SSSControlSurface
+{
+	bool clear;		// have we already started evaluating this point yet?
+	float depth;
+};
+
+typedef struct SSSPointData
+{
+	float lightIntensity;
+};
+
